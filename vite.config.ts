b31d53b942571a14044ad41cd9cf7ef6555e3bb6
VictorFortuna <victorfortuna1980@gmail.com>
2025-07-29 13:58:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for GitHub Pages
+  base: '/Telegram_Stars/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
